c40a6e4ccac53f3d0b08318ab14af1451875cc5c
Tamhee_Lee <tamhee0110@naver.com>
2023-02-07 22:08:55 +0900

    HashMap&TreeSet algorithm

---
+++ 04_HashMap&TreeSet/src/problems01/run/Main.java
@@ -0,0 +1,36 @@
+package problems01.run;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		String str = kb.next();
+		System.out.println(T.solution(n, str));
+		
+	}
+	
+	public char solution(int n, String s) {
+		char answer = ' ';
+		HashMap<Character, Integer> map = new HashMap<>();
+		for(char x : s.toCharArray()) {
+			map.put(x, map.getOrDefault(x, 0)+1);
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for(char key : map.keySet()) {
+			//System.out.println(key + " " +map.get(key));
+			if(map.get(key)> max) {
+				max = map.get(key);
+				answer = key;
+			}
+		}
+		
+		
+		return answer;
+	}
+
+}
